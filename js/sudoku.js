58cc4887d495d11f1a2ad189c8df86db760b5bb7
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2018-12-14 12:02:44 -0500

    First commit

---
+++ js/sudoku.js
@@ -0,0 +1,127 @@
+function solve() {
+  console.log('solve!');
+}
+
+function setupBoard() {
+  const board = document.querySelector('table');
+  for (let i = 0; i < 9; i++) {
+    const currRow = board.insertRow(-1);
+    for (let j = 0; j < 9; j++) {
+      const currCell = currRow.insertCell(-1);
+      const rowClass = (i % 3 === 0) ? 'top' : (i % 3 === 2) ? 'bottom' : null;
+      const colClass = (j % 3 === 0) ? 'left' : (j % 3 === 2) ? 'right' : null;
+      if (rowClass) {
+        currCell.classList.add(rowClass);
+      }
+      if (colClass) {
+        currCell.classList.add(colClass);
+      }
+      currCell.id = `row${i}col${j}`;
+      const inputBox = document.createElement('input');
+      inputBox.id = `row${i}col${j}input`;
+
+      inputBox.addEventListener('focus', function() {
+        this.select();
+      });
+
+      // force input to be integer between 1 and 9, move to next cell if good input is entered
+      inputBox.addEventListener('input', function () {
+        if (!this.value) {
+          return;
+        }
+        if (!Number.isInteger(+this.value) || +this.value < 1 || +this.value > 9) {
+          this.value = '';
+          this.classList.add('warning');
+          return;
+        }
+        this.value = this.value.trim().slice(0,1); // in case something like " 3.0" was copy-pasted
+        if (j !== 8) {
+          document.querySelector(`#row${i}col${j+1}input`).focus();
+        }
+        else if (i !== 8) {
+          document.querySelector(`#row${i+1}col0input`).focus();
+        }
+        else {
+          document.querySelector(`#row0col0input`).focus();
+        }
+      });
+
+      // board navigation controls:
+      inputBox.addEventListener('keyup', function(event) {
+        if (event.key === 'ArrowRight' || event.key === 'Right') {
+          if (j !== 8) {
+            document.querySelector(`#row${i}col${j+1}input`).focus();
+          }
+          else if (i !== 8) {
+            document.querySelector(`#row${i+1}col0input`).focus();
+          }
+          else {
+            document.querySelector(`#row0col0input`).focus();
+          }
+        }
+        if (event.key === 'ArrowLeft' || event.key === 'Left' || event.key === 'Backspace') {
+          if (j !== 0) {
+            document.querySelector(`#row${i}col${j-1}input`).focus();
+          }
+          else if (i !== 0) {
+            document.querySelector(`#row${i-1}col8input`).focus();
+          }
+          else {
+            document.querySelector(`#row8col8input`).focus();
+          }
+        }
+        if (event.key === 'ArrowUp' || event.key === 'Up') {
+          if (i !== 0) {
+            document.querySelector(`#row${i-1}col${j}input`).focus();
+          } else {
+            document.querySelector(`#row${8}col${j}input`).focus();
+          }
+        }
+        if (event.key === 'ArrowDown' || event.key === 'Down') {
+          if (i !== 8) {
+            document.querySelector(`#row${i+1}col${j}input`).focus();
+          } else {
+            document.querySelector(`#row${0}col${j}input`).focus();
+          }
+        }
+      });
+      currCell.appendChild(inputBox);
+    }
+  }
+}
+
+function setupClearButton() {
+  document.querySelector('#clear').addEventListener('click', () => {
+    const allRows = document.querySelectorAll('tr');
+    allRows.forEach(row => {
+      row.remove();
+    });
+    setupBoard();
+    setupBadInputWarning();
+  });
+}
+
+function setupSubmitButton() {
+  document.querySelector('#submit').addEventListener('click', solve);
+}
+
+function setupBadInputWarning() {
+  const allInputBoxes = document.querySelectorAll('input');
+  allInputBoxes.forEach(box => {
+    box.addEventListener('animationend', function() {
+      this.classList.remove('warning'); 
+    });
+  });
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', setupBoard);
+    document.addEventListener('DOMContentLoaded', setupClearButton);
+    document.addEventListener('DOMContentLoaded', setupSubmitButton);
+    document.addEventListener('DOMContentLoaded', setupBadInputWarning);
+} else {
+    setupBoard();
+    setupClearButton();
+    setupSubmitButton();
+    setupBadInputWarning();
+}
