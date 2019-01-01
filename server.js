16a3d33c07851ef09443373fde7acac56404fc95
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-01 11:48:29 -0500

    port is now set by environment if possible (or 3000)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 
 app.use(express.static(`${__dirname}/public`));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, (() => {
   console.log(`server is running on port ${port}`);
