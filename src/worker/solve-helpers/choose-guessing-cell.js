1d57ddd34b53d7108c3fe6d291042f3f0cb1cdfb
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-02-08 13:51:39 -0500

    refactor choose-guessing-cell.js

--- src/worker/solve-helpers/choose-guessing-cell.js
+++ src/worker/solve-helpers/choose-guessing-cell.js
@@ -1,12 +1,26 @@
 // choose a cell with no value and with fewest possVals left
 
 export default cellObjArray => {
-  const cellObjectsWithoutValue = [];
-  cellObjArray.forEach(cellObj => {
-    if (!cellObj.val) {
-      cellObjectsWithoutValue.push(cellObj);
+  let guessingCell;
+
+  for (const cellObj of cellObjArray) {
+
+    // move on to next cell if this one has a value
+    if (cellObj.val) {
+      continue;
     }
-  });
-  cellObjectsWithoutValue.sort((a, b) => a.possVals.length - b.possVals.length);
-  return cellObjectsWithoutValue[0];
+
+    // definitively choose cell if it has only 2 possVals (can't do better!)
+    if (cellObj.possVals.length === 2) {
+      guessingCell = cellObj;
+      break;
+    }
+
+    // tentatively choose cell if it's first candidate or better than current candidate
+    if (!guessingCell || cellObj.possVals.length < guessingCell.possVals.length) {
+      guessingCell = cellObj;
+    }
+  }
+
+  return guessingCell;
 };
